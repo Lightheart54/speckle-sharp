cb09b468ea2607da8443b9d6b9dd0f0efa1c52ab
Cullen Sarles <cullen.sarles@ryancompanies.com>
2024-05-28 13:34:56 -0500

    rework to only store metadata if necessary for roundtrip

--- ConnectorRhino/ConnectorRhino/ConnectorRhinoShared/ChimeraTransport/SpeckleChimeraAdapter.cs
+++ ConnectorRhino/ConnectorRhino/ConnectorRhinoShared/ChimeraTransport/SpeckleChimeraAdapter.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using Chimera;
+using Chimera.Data;
 using Chimera.DataAccess;
+using Chimera.Definitions;
+using JetBrains.Annotations;
 using Speckle.Core.Kits;
 using Speckle.Core.Models;
 
@@ -26,38 +29,36 @@ public static class SpeckleChimeraAdapter
     return chimeraBase;
   }
 
-  static Base ChimeraDataToSpeckle(IChimeraData chimeraData, ISpeckleConverter converter)
+  [CanBeNull]
+  static object ChimeraDataToSpeckle(IChimeraData chimeraData, ISpeckleConverter converter) =>
+    chimeraData switch
+    {
+      Chimera.Data.BaseGeometryData geoData => converter.ConvertToSpeckle(geoData.TypedValue),
+      Chimera.Data.SpeciesData sData => ChimeraSpeciesToSpeckle(sData, converter),
+      Chimera.Data.ListData lData => ChimeraListToSpeckle(lData, converter),
+      _ => chimeraData.GetValue()
+    };
+
+  static Base ChimeraSpeciesToSpeckle(Chimera.Data.SpeciesData speciesData, ISpeckleConverter converter)
   {
     var chimeraBase = new Base();
-    chimeraBase[CHIMERA_OBJECT_ID_KEY] = chimeraData.Definition.Uid;
-    chimeraBase["Description"] = chimeraData.Definition.Description;
-    object val = null;
-    switch (chimeraData)
+    chimeraBase[CHIMERA_OBJECT_ID_KEY] = speciesData.Key.First.Key.ToString();
+    chimeraBase["Description"] = speciesData.Definition.Description;
+    foreach (IChimeraData data in speciesData.Values)
     {
-      case Chimera.Data.BaseGeometryData geoData:
-      {
-        chimeraBase[CHIMERA_VALUE_KEY] = converter.ConvertToSpeckle(geoData.TypedValue);
-        break;
-      }
-      case Chimera.Data.SpeciesData sData:
-      {
-        foreach (IChimeraData childData in sData.Values)
-        {
-          chimeraBase[childData.Definition.Name] = ChimeraDataToSpeckle(childData, converter);
-        }
-        break;
-      }
-      case Chimera.Data.ListData lData:
-      {
-        for (int i = 0; i < lData.Count; i++)
-        {
-          chimeraBase[$"item_{i}"] = ChimeraDataToSpeckle(lData[i], converter);
-        }
-        break;
-      }
-      default:
-        chimeraBase[CHIMERA_VALUE_KEY] = chimeraData.GetValue();
-        break;  
+      chimeraBase[data.Definition.Name] = ChimeraDataToSpeckle(data, converter);
+    }
+
+    return chimeraBase;
+  }
+  
+  static Base ChimeraListToSpeckle(Chimera.Data.ListData listData, ISpeckleConverter converter)
+  {
+    var chimeraBase = new Base();
+    for (int i = 0; i < listData.Count; i++)
+    {
+      var data = listData[i];
+      chimeraBase[$"{data.Definition.Name}_[{i}]"] = ChimeraDataToSpeckle(data, converter);
     }
     return chimeraBase;
   }
@@ -68,51 +69,71 @@ public static class SpeckleChimeraAdapter
     foreach (var propPair in @object.GetMembers())
     {
       if(string.Equals(propPair.Key, CHIMERA_OBJECT_ID_KEY, StringComparison.InvariantCultureIgnoreCase)) continue;
-      if(propPair.Value is not Base speckObject) continue;
-      receivedData.Add(ParseSpeckleToChimeraData(speckObject, converter));
+      if(propPair.Value is not Base dataObject) continue;
+      var defIdObject = dataObject[CHIMERA_OBJECT_ID_KEY];
+      var attrId = Guid.Parse(dataObject[CHIMERA_OBJECT_ID_KEY] as string ?? string.Empty);
+      if (attrId == Guid.Empty) continue;
+      var def = SpeciesManager.GetAttribute(attrId);
+      if (def is not SpeciesDefinition sDef) continue;
+      var sData = sDef.CreateDefaultData() as SpeciesData;
+      ParseSpeckleToChimeraSpecies(sData, dataObject, converter);
+      
+      receivedData.Add(sData);
     }
     cuo.BatchWriteData(receivedData);
   }
 
-  static IChimeraData ParseSpeckleToChimeraData(Base speckleObject, ISpeckleConverter converter)
+  static void ParseSpeckleToChimeraData(IChimeraData data, object speckleData, ISpeckleConverter converter)
   {
-    var attrId = Guid.Parse(speckleObject[CHIMERA_OBJECT_ID_KEY] as string ?? string.Empty);
-    if (attrId == Guid.Empty) return null;
-    var def = SpeciesManager.GetAttribute(attrId);
-    var data = def.CreateDefaultData();
-    var val = speckleObject[CHIMERA_VALUE_KEY];
     switch (data)
     {
-      case Chimera.Data.BaseGeometryData geoData:
-        if (val is not Base baseVal) break;
-        data.SetValue(converter.ConvertToNative(baseVal));
+      case SpeciesData sData:
+        ParseSpeckleToChimeraSpecies(sData,speckleData as Base, converter);
         break;
-      case Chimera.Data.ListData lData:
-        {
-          Dictionary<int,IChimeraData> listItems = new();
-          foreach (var propPair in @speckleObject.GetMembers())
-          {
-            if(!propPair.Key.StartsWith("item_")) continue;
-            listItems[int.Parse(propPair.Key.Substring(5))] =
-              ParseSpeckleToChimeraData((Base)propPair.Value, converter);
-          }
-          foreach (var cData in listItems.OrderBy(x => x.Key).Select(x => x.Value))
-          {
-            lData.Add(cData);
-          }
-        }
+      case ListData lData:
+        ParseSpeckleToChimeraList(lData, speckleData as Base, converter);
         break;
-      case Chimera.Data.SpeciesData sData:
-        var sDef = def as Chimera.Definitions.SpeciesDefinition;
-        foreach (var childDef in sDef.GetAttributes())
-        {
-          sData[childDef.Uid] = ParseSpeckleToChimeraData((Base)speckleObject[childDef.Name], converter);
-        }
+      case BaseGeometryData geoData:
+        geoData.SetValue(converter.ConvertToNative(speckleData as Base));
         break;
       default:
-        data.SetValue(val);
+        data.SetValue(speckleData);
         break;
     }
-    return data;
+  }
+
+  static void ParseSpeckleToChimeraSpecies(SpeciesData sData, Base speckleObject, ISpeckleConverter converter)
+  {
+    foreach (var defChild in sData.Values)
+    {
+      ParseSpeckleToChimeraData(defChild, speckleObject[defChild.Definition.Name], converter);
+    }
+  }
+
+  public static void ParseSpeckleToChimeraList(ListData lData, Base speckleObject, ISpeckleConverter converter)
+  {
+    var speckleData = speckleObject.GetMembers();
+    #nullable enable
+    SortedDictionary<int, IChimeraData> parsedData = new();
+    foreach (KeyValuePair<string,object?> valuePair in speckleData)
+    {
+      if(!valuePair.Key.StartsWith(lData.Definition.Name)) continue;
+      //get the index
+      var indexStart = valuePair.Key.LastIndexOf('[');
+      var indexEnd = valuePair.Key.LastIndexOf(']');
+      if(indexStart == -1 || indexEnd == -1 || indexStart >= indexEnd) continue;
+      
+      var indexStr = valuePair.Key.Substring(indexStart + 1, indexEnd - indexStart - 1);
+      if (!int.TryParse(indexStr, out int index)) continue;
+      
+      var itemData = lData.GetDefaultItem();
+      ParseSpeckleToChimeraData(itemData, valuePair.Value, converter);
+      parsedData[index] = itemData;
+    }
+    //add the items in order to the data
+    foreach (var kvp in parsedData)
+    {
+      lData.Add(kvp.Value);
+    }
   }
 }
